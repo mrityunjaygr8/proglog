e3de9cf6bf8ac31629ae56cb9b8ef03df190f5bd
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2023-03-01 17:47:18 +0530

    chap-04 done

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs; [protobuf fish protoc-gen-go];
+    nativeBuildInputs = with pkgs; [protobuf protoc-gen-go protoc-gen-go-grpc];
 }
